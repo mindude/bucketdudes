1cc97ef526a3e8b4fb6e3ac49602d607ff1c4f55
mindude <eleal@cetys.edu.mx>
2016-08-27 21:46:36 -0700

    Finished with homework 2

--- files/JS_for_examples/get_version_of_s3.js
+++ files/JS_for_examples/get_version_of_s3.js
@@ -4,9 +4,19 @@ AWS.config.region = 'us-west-2';
 
 var YOUR_BUCKET = "bucketdudes"
 var s3 = new AWS.S3();
+var params = { Bucket: YOUR_BUCKET, Key:'index.html' };
+
+s3.getObject(params, function(err, data) 
+{ if (err) console.log(err, err.stack);
+else 
+console.log('This is how you get the "Website version"');
+console.log("Last-edit: ",data.LastModified); }); 
+
+
 s3.listBuckets(function(err, data) {
   if (err) { console.log("Error:", err); }
   else {
+console.log('This is how you get the "Bucket version"');
     for (var index in data.Buckets) {
       var bucket = data.Buckets[index];
 if(bucket.Name == YOUR_BUCKET){
